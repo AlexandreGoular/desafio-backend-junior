dce6ca8b3c0f861cc2fe8a4a037100ba5a91deb2
AlexandreGoular <xandeaugustogoulart@gmail.com>
2024-07-24 23:36:39 -0300

    Create repository for tasks

--- src/main/java/desafio_backend/junior/repository/TarefaRepository.java
+++ src/main/java/desafio_backend/junior/repository/TarefaRepository.java
@@ -1,4 +1,11 @@
 package desafio_backend.junior.repository;
 
-public interface TarefaRepository {
+import desafio_backend.junior.model.TarefaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<TarefaModel, UUID> {
 }
